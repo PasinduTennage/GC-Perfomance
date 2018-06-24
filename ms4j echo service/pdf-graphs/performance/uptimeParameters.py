cfadf0fd2da5bd6230c39cc876d6046f32f2e9a0
PasinduTennage <pasindu.13@cse.mrt.ac.lk>
2018-06-24 22:21:37 +0530

    Rearranged Directories

--- ms4j echo service/pdf-graphs/performance/uptimeParameters.py
+++
@@ -1,9 +0,0 @@
-def getLoadAverages(uptime_file_output):
-    load_averages = {}
-    file = open(uptime_file_output, "r")
-    line = file.readlines()[0].strip()
-    content = line.split(",")
-    load_averages[15] = content[len(content)-1].strip()
-    load_averages[5] = content[len(content) - 2].strip()
-    load_averages[1] = content[len(content) - 3].replace("load average:", "").strip()
-    return load_averages
